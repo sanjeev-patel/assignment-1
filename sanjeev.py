ff3ae860ee080425a432d212e65c53e9f5c7c6b9
sanjeev-patel <78227819+sanjeev-patel@users.noreply.github.com>
2021-01-30 00:26:52 +0530

    Add files via upload

---
+++ sanjeev.py
@@ -0,0 +1,5 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 16:33:24) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("my name is SANJEEV PATEL")
+my name is SANJEEV PATEL
+>>> 
